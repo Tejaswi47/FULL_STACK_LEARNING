c0cd1586c8d0d7978dff1ed012abf0f78f635b73
Tejaswi47 <[bodapatitejaswipratap8@gmail.com]>
2024-10-26 14:10:58 +0530

    The updated one

--- WEEK-1/ASYNC-AWAIT-PROMISES.js
+++ WEEK-1/ASYNC-AWAIT-PROMISES.js
@@ -16,5 +16,38 @@ setTimeout(calculatesum, 1000);
 
 console.log("Hi there this is the last line of the code");
 
+// -------------------------------------------------------------------------------------
+
+function pratapcalculate(){
+    return new Promise(function(resolve){
+        let sum = 0;
+        for(let i=0; i<1000000;i++) {
+            sum = sum + i
+        }
+        resolve(sum);
+    });
+}
+
+function onThen(data){
+    console.log("The total sum for 1000 numbers is " + data);
+}
+
+pratapcalculate().then(onThen);
 
 
+// -------------------------------------------------------------------------------------
+
+async function pratapcalculateversion2(){
+    let sum = 0;
+    for(let i=0; i< 10; i++){
+        sum = sum + i;
+    }
+    return sum;
+}
+
+async function main(){
+    const sum_v2 = await pratapcalculateversion2();
+    console.log("The total sum for 10 numbers is " + sum_v2);
+}
+
+main();
